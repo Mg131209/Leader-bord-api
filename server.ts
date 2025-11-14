10df7f67a7ee068477fdd4fc35648d2cbe8826a6
Galli Mio <Mio.Galli@swisscom.com>
2025-11-14 09:26:28 +0100

    initial commit

---
+++ server.ts
@@ -0,0 +1,73 @@
+import express, { request, response } from "express";
+import { Database } from "bun:sqlite";
+
+const db = new Database("leader-board.sqlite");
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // <- WICHTIG
+
+function generateId() {
+  return Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, "0");
+}
+
+function userExists(id: string) {
+  const data = db.query("SELECT id FROM Users WHERE id = ?").get(id);
+  console.log("selectd id: " + data);
+  if (data !== null && data.id === id) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+db.query(
+  "CREATE TABLE IF NOT EXISTS Users (name TEXT, score INTEGER  DEFAULT 0, id TEXT PRIMARY KEY)"
+).run();
+app.get("/", (request, response) => {
+  const data = db.query("SELECT name, score FROM USERS").get();
+  response.send(data);
+});
+
+app.get("/score", (request, response) => {
+  if (userExists(request.body.id)) {
+    const data = db
+      .query("SELECT score FROM Users WHERE id = ?")
+      .get(request.body.id);
+    if (data) {
+      response.send(data);
+    } else {
+      response.status(404).send();
+    }
+  } else {
+    response.status(401).send();
+  }
+});
+app.post("/score", (request, response) => {
+  if (userExists(request.body.id)) {
+    db.query("UPDATE Users SET score = score + 1 WHERE id = ?;").run(
+      request.body.id
+    );
+    const data = db
+      .query("SELECT score FROM Users WHERE id = ?")
+      .get(request.body.id);
+    response.status(200).send(data);
+  } else {
+    response.status(401).send();
+  }
+});
+
+app.post("/users", (request, response) => {
+  const id = generateId();
+
+  db.query("INSERT INTO users (name, id) VALUES (?, ?)").run(
+    request.body.name,
+    id
+  );
+
+  response.send({ id });
+});
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
